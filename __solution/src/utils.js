fa4fdd94d7f8695765828cd38528b04a7e7b14c8
Joshua Comeau <joshwcomeau@gmail.com>
2020-03-24 08:08:24 -0400

    WIP

--- __solution/src/utils.js
+++
@@ -1,6 +0,0 @@
-export const range = n => Array.from(Array(n).keys());
-
-export const sample = arr => arr[Math.floor(Math.random() * arr.length)];
-
-export const random = (min, max) =>
-  Math.floor(Math.random() * (max - min)) + min;
