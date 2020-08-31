e02421b9bec434b0ef401b7f063bd6240f7ce26c
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2020-08-31 19:48:26 -0400

    Initial setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import GlobalStyles from '../GlobalStyles';
+
 const App = () => {
-  return <Router>TODO</Router>;
+  return <Router>
+    TODO
+    <GlobalStyles />
+  </Router>;
 };
 
 export default App;
